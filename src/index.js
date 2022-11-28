14977e91a69a91519c8dda56893b0093e8da889b
Viktoria Domashchuk <domashchukviktoria@gmail.com>
2022-11-28 22:58:21 +0200

    start markup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+console.log(axios);
+import Notiflix from 'notiflix';
+console.log(Notiflix);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(SimpleLightbox);
